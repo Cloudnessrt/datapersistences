c32dd8057741e8e30bc1e8a69ceb54753afb2cfe
cloudness <tanghecaijing@outlook.com>
2018-09-04 14:28:30 +0800

    sql 日志

--- src/main/java/com/singularity/datapersistence/aop/JdbcAop.java
+++ src/main/java/com/singularity/datapersistence/aop/JdbcAop.java
@@ -1,29 +1,45 @@
 package com.singularity.datapersistence.aop;
 
+import com.alibaba.fastjson.JSON;
+import com.singularity.datapersistence.config.Config;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+@Order(2147483647)
 @Aspect
 @Component
 public class JdbcAop {
+
     @Pointcut("execution(public * org.springframework.jdbc.core.*.*(..))")
-    public void webLog(){}
+    public void sqlLog(){}
 
-    @Before("webLog()")
+    /**
+     * sql监控
+     * @param joinPoint
+     * @throws Throwable
+     */
+    @Before("sqlLog()")
     public void deBefore(JoinPoint joinPoint) throws Throwable {
-        Object[] args = joinPoint.getArgs();//参数
-        // 记录下请求内容
-        System.out.println("URL : " + request.getRequestURL().toString());
-        System.out.println("HTTP_METHOD : " + request.getMethod());
-        System.out.println("IP : " + request.getRemoteAddr());
-        System.out.println("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        System.out.println("ARGS : " + Arrays.toString(joinPoint.getArgs()));
-
+        Config config=Config.getInstance();
+        if(config.isPrintSql()){
+            Object[] args = joinPoint.getArgs();//参数
+            Signature signature = joinPoint.getSignature();//此处joinPoint的实现类是MethodInvocationProceedingJoinPoint
+            MethodSignature methodSignature = (MethodSignature) signature;//获取参数名
+            HashMap map=new HashMap();
+            for(int i=0;i<methodSignature.getParameterNames().length;i++){
+                map.put(methodSignature.getParameterNames()[i],args[i]==null? "":JSON.toJSONString(args[i]));
+            }
+            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis())+" jdbcSqlLog--> "+map.toString()+"\n");
+        }
     }
+
 
 }
